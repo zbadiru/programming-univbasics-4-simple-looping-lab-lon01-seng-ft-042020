532b9008bffac6a32da10814c07f21d8cf99fa98
Zakari Badiru <zbadiru@live.co.uk>
2020-03-31 15:20:33 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,7 +1,7 @@
-def return_string_array(array)
+def loop_message_five_times(message)
   count = 0
-  while count < array.length do
- 
+  while count < 5 do
+    puts message
     count += 1
   end
 end
