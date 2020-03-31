554c6fd221023c671b9cbd6346971b92d973abcc
Zakari Badiru <zbadiru@live.co.uk>
2020-03-31 15:19:06 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,6 +1,14 @@
 
 def return_string_array(array)
   count = 0
+  while count < array.length do
+    array[count].to_s
+    count += 1
+  end
+end
+
+def return_string_array(array)
+  count = 0
   new_array = []
   while count < array.length do
     new_array.push(array[count].to_s) 
